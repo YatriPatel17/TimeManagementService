e2806910db542d157ea94392e6e08eeda6760970
Yatripatel17 <yatripatel1710@gmail.com>
2026-01-30 23:31:44 -0500

    Create Models folder and then add TimeEntry in model folder with validation.

---
+++ Models/TimeService.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManagementService.Models
+{
+    public class TimeEntry
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Task name is required")]
+        [StringLength(100, MinimumLength = 4)]
+        public string TaskName { get; set; } = string.Empty;
+        
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Due date is required!")]
+        public DateTime DueDate { get; set; }
+
+        public string? TaskCategory { get; set; }
+
+        [Required(ErrorMessage = "Task status is required")]
+        public string TaskStatus { get; set; } = "Pending";
+
+        [Required(ErrorMessage = "Time spent in this task is required")]
+        [Range(0, 10000, ErrorMessage = "Time spent must be in 0 to 10000 minutes")]
+        public int TimeSpent { get; set; } = 0;
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
